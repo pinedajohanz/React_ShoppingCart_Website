38a594c2876689643687594296e97b5fe608f5e2
Johanz Pineda <johanzpineda@gmail.com>
2023-03-13 20:05:45 +0800

    feat: cart-item input functionalities

--- client/src/pages/cart/cart.jsx
+++ client/src/pages/cart/cart.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { ShopContext } from '../../context/shop-context'
+import { PRODUCTS } from '../../products'
+import { CartItem } from './cart-item'
+import './cart.css'
 
 export const Cart = () => {
+    const { cartItems } = useContext(ShopContext)
+
   return (
-    <div>cart</div>
+    <div className='cart'>
+        <div> 
+            <h1> Your Cart </h1> 
+        </div>
+        {/* list of the cart items */}
+        <div className='cartItems'>
+            {PRODUCTS.map((product) => {
+                if (cartItems[product.id] !== 0 ) {
+                   return <CartItem data={product}/> 
+                }
+            })}
+        </div>
+    </div>
   )
 }
