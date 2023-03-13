9039e2cefadd793bc9d7e48ac77faa0104a0667d
Johanz Pineda <johanzpineda@gmail.com>
2023-03-13 12:59:44 +0800

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import { Navbar } from "./components/navbar"
+import { Shop } from './pages/shop/shop'
+import { Cart } from './pages/cart/cart'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        {/* navbar avail in every page */}
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />}/>
+          <Route path="cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/products.js
@@ -0,0 +1,24 @@
+import iphone from './assets/iphone.jpg'
+import tv from './assets/tv.jpg'
+import laptop from './assets/laptop.png'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "iPhone 14 Pro Max",
+        price: '50,000',
+        productImage: iphone,
+    },
+    {
+        id: 2,
+        productName: "Samsung Television",
+        price:' 80,000',
+        productImage: tv,
+    },
+    {
+        id: 3,
+        productName: "Laptop Zenbook",
+        price:' 60,000',
+        productImage: laptop,
+    }
+]
